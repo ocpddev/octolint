14af0055cf422b38e2063dc20e31871c7eb79048
Sola <sola@ocpd.dev>
2023-10-17 17:53:24 +0800

    feat: allow configuring private key directly from env

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,11 @@
-import * as fs from 'fs';
-
 const portStr = process.env.PORT;
 const port = portStr ? parseInt(portStr) : 3000;
 
 const appId = process.env.APP_ID;
 if (!appId) throw new Error('APP ID is not configured');
 
-const privateKeyPath = process.env.PRIVATE_KEY;
-if (!privateKeyPath) throw new Error('Private key is not configured');
-
-const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const privateKey = process.env.PRIVATE_KEY;
+if (!privateKey) throw new Error('Private key is not configured');
 
 const webhookSecret = process.env.WEBHOOK_SECRET;
 if (!webhookSecret) throw new Error('Webhook secret is not configured');
